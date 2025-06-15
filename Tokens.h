460512ae328f39c49df865be81ee0c3e2930ea83
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-06-15 16:25:57 +0200

    main

---
+++ Tokens.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#pragma once
+
+
+enum class Token_type {
+    LineBreak,
+    Unkown,
+    String,
+    Hash,
+
+    Print,
+    Bracket_open,
+    Bracket_close,    
+    };
+        struct Token
+    {
+        Token_type type;
+        std::string lexem;
+        int line;
+
+        Token(Token_type t, std::string& lex, int ln ) {
+            type = t;
+            lexem = lex;
+            line = ln;
+        }
+    };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,84 @@
 #include <iostream>
+#include <Tokens.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Lexer {
+    public:
+        Lexer(const string& source) {
+            src = source;
+            i = 0;
+            line = 1;
+        };
+        vector<Token> lex(){
+            tokens_vector.clear();
+            get_token_function();
+            return tokens_vector;
+        };
+    private:
+        string src;
+        vector<Token> tokens_vector;
+        size_t i = 0;
+        int line = 1;
+
+        void get_token_function () {
+            while (i < src.length())
+    {
+        char f = src[i];
+
+        if (f == '#')
+        {
+            Token_type type = Token_type::Hash;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+        if (f == ':')
+        {
+            Token_type type = Token_type::Print;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+        if (f == '"')
+        {
+            Token_type type = Token_type::String;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+        if (f == '(')
+        {
+            Token_type type = Token_type::Bracket_open;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+        if (f == ')')
+        {
+            Token_type type = Token_type::Bracket_close;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+        else {
+            Token_type type = Token_type::Unkown;
+            string str = string(1, f);
+            tokens_vector.push_back(Token(type, str, line));
+            i++;
+            continue;
+        }
+    }
+        }
+};
 
 int main() {
+    cout << "Läuft";
     return 0;
 }
