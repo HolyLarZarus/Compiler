c37839c4e5da0bb7c6ed183dee082315ee08dbb2
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-06-15 16:59:07 +0200

    main

--- main.cpp
+++ main.cpp
@@ -78,6 +78,75 @@ class Lexer {
         }
 };
 
+class Parser {
+    public:
+        Parser(vector<Token>& token) {
+            tokens = token;
+        }
+    private:
+        vector<Token> tokens;
+        size_t i = 0;
+        Token_type crnt_type;
+        void run () {
+            
+            while (i < tokens.size())
+            {
+                crnt_type = tokens[i].type;
+                if (crnt_type == Token_type::Print)
+                {
+                    predict(Token_type::Bracket_open);
+                    i+=2;
+                    if (crnt_type == Token_type::String)
+                    {
+                        i++;
+                        handlestring();
+                        
+                    }
+                    else {
+                        cout << "Syntax Error in Line" << tokens[i].line;
+                    }
+                    i--;
+                    predict(Token_type::Bracket_close);
+                    i+=2;
+                    
+                }
+
+                if (crnt_type == Token_type::Hash) {
+                    i++;
+                    while (crnt_type != Token_type::LineBreak) {
+                        i++;
+                    }
+                    i++;
+                }
+            }
+            
+        }
+
+        void handlestring() {
+
+            while (crnt_type != Token_type::String && crnt_type != Token_type::LineBreak)
+            {
+                i++;
+            }
+                predict(Token_type::String);
+                i++;
+
+        }
+
+        Token_type predict(Token_type current) {
+
+            Token_type prediction = tokens[i+1].type;
+            
+            if (current != prediction)
+            {   
+                cout << "Syntax Error!";
+                exit(1);
+            }
+            return prediction;
+        }
+};
+
+
 int main() {
     cout << "Läuft";
     return 0;
