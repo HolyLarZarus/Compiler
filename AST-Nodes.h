9540428d5f0b9d98d4cb9c7e9c612a2bd3ab3663
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-06-18 22:01:33 +0200

    Header file

--- AST-Nodes.h
+++ AST-Nodes.h
@@ -15,5 +15,30 @@ struct AST_Nodes
     Node_type type;
     string variable_name;
     string string_content;
+
+
+    AST_Nodes (string& variable, bool isdeclared) {
+        string name = variable;
+        Node_type type = Node_type::Variable;
+        bool declared = isdeclared;
+    }
+    AST_Nodes (string& con) {
+        string content = con;
+        Node_type type = Node_type::StringContent;
+    }
+    AST_Nodes () {
+        Node_type type = Node_type::Print;
+    }
+    AST_Nodes () {
+        Node_type type = Node_type::Comment;        
+    }
+
+    /*
     
+    
+    Wichtig!!!
+    Alle kontruktoren in einen Zusammenfassen mit inneren If statements
+    
+    
+    */
 };
